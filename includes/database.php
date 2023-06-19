34d9f66e751579a5bf2d8ec56e78bd3776b195b3
Andrey <73118147+TrueMage@users.noreply.github.com>
2023-06-19 12:14:04 +0200

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -2,8 +2,8 @@
 
   require 'rb.php';
   
-  R::setup( 'mysql:host=localhost;dbname=u1556751_default',
-        'u1556751_default', '239sGcnWGyMW8Hb5' ); //for both mysql or mariaDB
+  R::setup( 'mysql:host=localhost;dbname=',
+        '', '239sGcnWGyMW8Hb5' ); //for both mysql or mariaDB
   
   session_start();
  ?>
